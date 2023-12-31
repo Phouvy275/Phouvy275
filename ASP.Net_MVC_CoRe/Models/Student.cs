0d001152d334e25c291f4aff766ad5bec8747565
Phouvy275 <phuongvy0542@gmail.com>
2023-12-31 18:27:16 +0700

    .

---
+++ ASP.Net_MVC_CoRe/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.Net_MVC_CoRe.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public String Name{ get; set; }
+        public DateTime Birthday { get; set; }
+        public int Address_ID { get; set; }
+        public String Address_Full { get; set; }
+
+    }
+}
